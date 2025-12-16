b2625d3c604c457b5d4954f15b9ce9050a74d0d3
Tyler <139372160+tpinzzz@users.noreply.github.com>
2025-12-16 17:31:27 -0500

    feat: implement basic CRUD endpoints for accounts

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.routers import admin
 
 app = FastAPI(title="SchedulerRX API")
 
@@ -16,6 +17,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.router.include_router(admin.router, prefix="/api/v1", tags=["admin"])
 
 @app.get("/")
 def read_root():
